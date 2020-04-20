de7dd6bbb219deb366bae8470d29e3a44ab9bc76
didier <didier@dfr.ch>
2020-04-20 17:08:10 +0200

    Switch to hapi eslint

--- example/app.js
+++ example/app.js
@@ -6,49 +6,50 @@ const cookieParamHandler = require('@distalog/cookie-param-handler').Handler;
 const cssHandler = require('@distalog/cookie-css').Handler;
 
 const start = async () => {
-  const server = Hapi.server({
-    host: 'localhost',
-    port: 3000,
-    debug: {
-      request: '*',
-      log: '*',
-    },
-    routes: {
-      log: {
-        collect: true,
-      },
-      files: {
-        relativeTo: '/',
-      },
-    },
-  });
 
-  await server.register(require('@hapi/inert'));
-
-  server.route([
-    {
-      method: 'GET',
-      path: '/css',
-      handler: cssHandler(),
-    }, {
-      method: '*',
-      path: '/static/{param?}',
-      options: {
-        pre: [cookieParamHandler({isSecure: false,
-          // Make sure path points to CSS endpoint
-          path: '/css',
-        })],
-      },
-      handler: {
-        directory: {
-          path: Path.join(__dirname, 'static'),
-          listing: true,
+    const server = Hapi.server({
+        host: 'localhost',
+        port: 3000,
+        debug: {
+            request: '*',
+            log: '*'
         },
-      },
-    },
-  ]);
-  await server.start();
-  console.log('Server running at:', server.info.uri);
+        routes: {
+            log: {
+                collect: true
+            },
+            files: {
+                relativeTo: '/'
+            }
+        }
+    });
+
+    await server.register(require('@hapi/inert'));
+
+    server.route([
+        {
+            method: 'GET',
+            path: '/css',
+            handler: cssHandler()
+        }, {
+            method: '*',
+            path: '/static/{param?}',
+            options: {
+                pre: [cookieParamHandler({ isSecure: false,
+                    // Make sure path points to CSS endpoint
+                    path: '/css'
+                })]
+            },
+            handler: {
+                directory: {
+                    path: Path.join(__dirname, 'static'),
+                    listing: true
+                }
+            }
+        }
+    ]);
+    await server.start();
+    console.log('Server running at:', server.info.uri);
 };
 
 start();
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 'use strict';
 
-exports.Handler = function(cookieRegex = '^css_') {
-  return function( cookieRegex, request, h ) {
-    let output = '';
-    for (const k in request.state) {
-      if (k.match(cookieRegex)) {
-        output += request.state[k]+'\n';
-      }
-    }
-    const response = h.response(output);
-    response.type('text/css');
-    return response;
-  }.bind(undefined, cookieRegex);
+exports.Handler = function (cookieRegex = '^css_') {
+
+    return function ( cookieRegexParam, request, h ) {
+
+        let output = '';
+        for (const k in request.state) {
+            if (k.match(cookieRegexParam)) {
+                output += request.state[k] + '\n';
+            }
+        }
+
+        const response = h.response(output);
+        response.type('text/css');
+        return response;
+    }.bind(undefined, cookieRegex);
 };
